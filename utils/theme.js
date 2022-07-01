a7b7a77db31a841eb82de2df49bd02616abe741b
chonprakun1 <chonprakun@riverpark.co.th>
2022-07-01 22:35:33 +0700

    💄ui :file set theme color

---
+++ utils/theme.js
@@ -0,0 +1,19 @@
+import * as _colors from "@mui/material/colors";
+
+export const colors = {
+  primary: "#0096a9",
+  "primary.100": "rgba(0, 150, 169, .1)",
+  "primary.200": "rgba(0, 150, 169, .2)",
+  "primary.300": "rgba(0, 150, 169, .3)",
+  "primary.400": "rgba(0, 150, 169, .4)",
+  "primary.500": "rgba(0, 150, 169, .5)",
+  "primary.600": "rgba(0, 150, 169, .6)",
+  "primary.700": "rgba(0, 150, 169, .7)",
+  "primary.800": "rgba(0, 150, 169, .8)",
+  "primary.900": "rgba(0, 150, 169, .9)",
+  secondary: "#D3D800",
+  white: "#FFFFFF",
+  black: "#000000",
+  highlight: "#FF7C2C",
+  ..._colors,
+};
